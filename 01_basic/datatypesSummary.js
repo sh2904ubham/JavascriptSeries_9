ca9a022bd41c05af2cb39bdeb64840f712ba84bb
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-26 05:36:46 +0000

    Stack and Heap Memory

--- 01_basic/datatypesSummary.js
+++ 01_basic/datatypesSummary.js
@@ -13,3 +13,20 @@ const myFunc=function(){
     console.log("Hello World");
     
 }
+/////****** */
+//stack(Primitive),Heap(Non-Primitive)
+let myYouTubename="Shubham.com"
+let anothername=myYouTubename;
+anothername="chai"
+console.log(myYouTubename);
+
+console.log(anothername);
+let userOne={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+let userTwo=userOne
+userTwo.email="Shubham@gm.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
