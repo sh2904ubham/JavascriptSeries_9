6ac677ee66ef257f5396f3ce34978ee0de6b3105
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-26 05:24:58 +0000

    ChanheInLocationofDFile

---
+++ 01_basic/datatypesSummary.js
@@ -0,0 +1,15 @@
+//# Primitive
+//7 Types: String,Number, Boolean,null,undefined,Symbol,BigInt
+
+//Non Primitive or Reference Types
+//Arrays,Objects,Functions
+//
+const heroes=["shaktiman","naagraj", "doga"];
+let myObj={
+    name:"hitesh",
+    age:22
+}
+const myFunc=function(){
+    console.log("Hello World");
+    
+}
