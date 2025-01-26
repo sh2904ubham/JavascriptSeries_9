12f8e3b1a2ea1bfa8a6c978123a9768ffaab9834
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-26 04:55:52 +0000

    conversiom

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -1,12 +1,12 @@
-let score="33abc"
-let score1=null;
-console.log(typeof score);
-console.log(typeof(score))
-let valueInNumber= Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
-console.log(Number(score1))
-console.log(typeof Number(score1))
+// let score="33abc"
+// let score1=null;
+// console.log(typeof score);
+// console.log(typeof(score))
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+// console.log(Number(score1))
+// console.log(typeof Number(score1))
 
 //"33"=>33
 //"33abc"=>NaN
@@ -16,3 +16,17 @@ console.log(typeof Number(score1))
 //bollean conversion 1=>true ; 0=>false 
 //empty strings=>false;
 // "shubham"=>true
+//******OPERATIONS */
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+// console.log(+true);
+// console.log(+"");
+let num1, num2, num3
+num1 = num2= num3 =2+2
+let gameCounter=100;
+gameCounter++
+console.log(gameCounter);
+
+
