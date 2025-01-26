8b67d58ed5792742be2c9d03d98b9370875dee29
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-26 05:51:55 +0000

    String

---
+++ 01_basic/05_String.js
@@ -0,0 +1,7 @@
+const name="Shubham"
+const repoCount=50
+console.log(name+repoCount+"value");
+//outdated above
+//new method
+console.log(`My name is ${name} and my repoCount is ${repoCount} value`);
+const gameName=new String("Shubham");
