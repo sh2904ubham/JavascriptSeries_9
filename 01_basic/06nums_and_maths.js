d1062f5d4e0859cf5d836117190bd4bd095b6b0d
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-27 11:04:40 +0000

    Some additional

--- 01_basic/06nums_and_maths.js
+++ 01_basic/06nums_and_maths.js
@@ -7,3 +7,15 @@ console.log(balance.toString().length);
 console.log(balance.toFixed(2));
 const otherNumber=23.8966
 console.log(otherNumber.toPrecision(3));
+const hundred =1000000
+console.log(hundred.toLocaleString('en-IN'));
+console.log(Number.MAX_VALUE);
+
+//////////////////*/////////Maths
+console.log(Math.tan(45));
+console.log(Math.floor(4.9));
+console.log((Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
