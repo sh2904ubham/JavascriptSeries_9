4fa9783d483de0fca11068d46a299ea72af016a8
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-27 15:05:54 +0000

    Object2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,46 @@
+const tinderUser=new Object()
+tinderUser.id="123abc"
+tinderUser.name="Sam"
+tinderUser.isLoggedIn=false;
+
+//console.log(tinderUser);
+const regularuser={
+    email:"Some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Shubham",
+            lastname:"Ssss"
+        }
+    }
+}
+//console.log(regularuser.fullname.userfullname.firstname)
+const obj1={1:"a",
+    2:"b"
+}
+const obj2={3:"a",4:"b"}
+const obj3={obj1,obj2}
+//console.log(obj3);
+const obj4=Object.assign({},obj1,obj2);
+//console.log(obj4);
+const obj5={...obj1,...obj2}
+//console.log(obj5);
+const users=[{
+    id:1,
+    email:"S@gmail.com"
+},
+{
+idn:5
+},
+{}]
+//console.log(users[0].email);
+//console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
