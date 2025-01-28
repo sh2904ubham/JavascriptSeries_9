3c1dea80c0fc164f4f09cd4b708278ab8a812146
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-28 06:09:29 +0000

    Scope 2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,3 +4,15 @@ var c=30
 // console.log(a);
 // console.log(b);
 // console.log(c);
+function one(){
+    const username="Shubham"
+    function two()
+    {
+        const website="YouTube"
+        console.log(username);
+        
+    }
+   // console.log(website);
+    two();
+}
+one();
