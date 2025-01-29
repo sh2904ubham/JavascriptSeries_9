c6749cbf8703212bf8f878eccdb4d1ab7c77cc37
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-29 02:40:56 +0000

    truthy and control flow

---
+++ 04_ControlFlow/02_Switch.js
@@ -0,0 +1,37 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month=3;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("APR");
+        break;
+    case 5:
+        console.log("MAY");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("Jul");
+        break;
+
+    default:
+        console.log("No");
+        
+        break;
+}
---
+++ 04_ControlFlow/03_truthy.js
@@ -0,0 +1,38 @@
+const userEmail="SS@gmail.com"
+if(userEmail)
+{
+    console.log("GOt user email");
+    
+}
+else{
+    console.log("No user");
+    
+}
+//falsy values
+//false,0,-0,0m,""",null,undefined,NaN
+//truthy
+//"0",'false' " ",[],{},function(){}
+const emptyOj={
+
+}
+// if(userEmail.length===0){
+
+// for array}
+
+//for object
+if(Object.keys(emptyOj).length===0)
+{
+    console.log("Object");
+    
+}
+//Nulllish Coalescing Operator(??): null defined
+let val1;
+//val1=5??10
+val1=null??10
+
+console.log(val1);
+//Ternary Operator
+const iceTeaPrice=100
+iceTeaPrice>=80?console.log("less than 80"):console.log("more than 80");
+;
+
