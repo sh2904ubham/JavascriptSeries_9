5d4a4c016f2b61770ef217d49b452098b5255c1b
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-29 02:19:48 +0000

    One

---
+++ 04_ControlFlow/01_one.js
@@ -0,0 +1,31 @@
+// // // //if 
+// // // const isUserloggedIn=40;
+// // // if(isUserloggedIn===41)
+// // // {
+// // // console.log("Shubham");
+// // // }
+// // // else{
+// // //     console.log("Sh");
+    
+// // // }
+// // const score=200
+// // if(score>100)
+// // {
+// //     const power ="fly";
+// //     console.log(`user power: ${power}`);
+    
+// // }
+// // console.log(`user power:${power}`);
+// const balance=1000;
+// if(balance<500)
+// {
+// console.log("less than");
+// } else if(balance<750)
+// {
+//     console.log("less than 750");
+    
+// }
+// else{
+//     console.log(balance);
+    
+// }
