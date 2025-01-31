e52a6461604bf9458187a8ad121e348ddaa4066d
sh2904ubham <121863397+sh2904ubham@users.noreply.github.com>
2025-01-31 12:31:47 +0000

    control flow

--- 04_ControlFlow/01_one.js
+++ 04_ControlFlow/01_one.js
@@ -1,31 +1,35 @@
-// // // //if 
-// // // const isUserloggedIn=40;
-// // // if(isUserloggedIn===41)
+// // // // //if 
+// // // // const isUserloggedIn=40;
+// // // // if(isUserloggedIn===41)
+// // // // {
+// // // // console.log("Shubham");
+// // // // }
+// // // // else{
+// // // //     console.log("Sh");
+    
+// // // // }
+// // // const score=200
+// // // if(score>100)
 // // // {
-// // // console.log("Shubham");
-// // // }
-// // // else{
-// // //     console.log("Sh");
+// // //     const power ="fly";
+// // //     console.log(`user power: ${power}`);
     
 // // // }
-// // const score=200
-// // if(score>100)
+// // // console.log(`user power:${power}`);
+// // const balance=1000;
+// // if(balance<500)
 // // {
-// //     const power ="fly";
-// //     console.log(`user power: ${power}`);
+// // console.log("less than");
+// // } else if(balance<750)
+// // {
+// //     console.log("less than 750");
     
 // // }
-// // console.log(`user power:${power}`);
-// const balance=1000;
-// if(balance<500)
-// {
-// console.log("less than");
-// } else if(balance<750)
-// {
-//     console.log("less than 750");
+// // else{
+// //     console.log(balance);
     
-// }
-// else{
-//     console.log(balance);
-    
+// // }
+// else
+// {
+
 // }
